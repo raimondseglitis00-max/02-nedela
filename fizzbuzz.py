db8149e4879413da668e5c8ebd7cc34e21b393ad
Raimonds Eglitis <raimondseglitis00@gmail.com>
2026-03-08 17:17:53 +0200

    4.uzdevuma izpilde ar fizzbuzz un jazz

---
+++ fizzbuzz.py
@@ -0,0 +1,35 @@
+# fizzbuzz.py
+import sys
+
+# pārbauda vai ir ievadīts N
+if len(sys.argv) < 2:
+    print("Lietošana: python fizzbuzz.py N")
+    sys.exit()
+
+# mēģina pārvērst argumentu par skaitli
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("Kļūda: N jābūt skaitlim!")
+    sys.exit()
+
+# cikls no 1 līdz N
+for i in range(1, N + 1):
+
+    # vispirms pārbauda abus
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=", ")
+
+    # tad katru atsevišķi
+    elif i % 3 == 0:
+        print("Fizz", end=", ")
+
+    elif i % 5 == 0:
+        print("Buzz", end=", ")
+
+    # bonuss – ja dalās ar 7
+    elif i % 7 == 0:
+        print("Jazz", end=", ")
+
+    else:
+        print(i, end=", ")
